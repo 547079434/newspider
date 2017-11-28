a5ed2803f12c28548920c6bd27faad911be78db4
ylwu <547079434@qq.com>
2017-11-28 08:55:45 +0800

    no message

--- go.py
+++ go.py
@@ -2,5 +2,5 @@ import subprocess
 import time
 
 while True:
-	subprocess.call('cd news && scrapy crawl baidunews && scrapy crawl sinanews', shell=True)
+	subprocess.call('cd d://project/newspider/news && scrapy crawl baidunews && scrapy crawl sinanews', shell=True)
 	time.sleep(3600)
