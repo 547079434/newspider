0ea04a02e71f024abe5455371ee11a19d9435c2c
ylwu <547079434@qq.com>
2018-09-07 13:44:03 +0800

    导出excel

--- backstage/backstage/adminx.py
+++ backstage/backstage/adminx.py
@@ -3,6 +3,10 @@ import xadmin
 from xadmin.views.base import CommAdminView
 from xadmin.plugins.themes import ThemePlugin
 from .models import *
+from backstage.settings import MEDIA_ROOT
+from django.http import HttpResponseRedirect
+import xlwt
+import os
 
 class BaseSetting(object):
     enable_themes = True
@@ -38,4 +42,24 @@ class NewsAdmin(object):
     search_fields = ['title','url']
     list_filter = ['keys','publish_at']
     list_export = ['xls','xml','csv','json']
+    actions = ['export_excel']
+
+    def export_excel(self, request, queryset):
+        workbook = xlwt.Workbook()
+        sheet = workbook.add_sheet('sheet1', cell_overwrite_ok=True)
+        sheet.write(0, 0, u'标题')
+        sheet.write(0, 1, u'链接')
+        sheet.write(0, 2, u'发布时间')
+        sheet.write(0, 3, u'关键词')
+        for i,q in enumerate(queryset):
+            sheet.write(i+1, 0, q.title)
+            sheet.write(i+1, 1, q.url)
+            sheet.write(i+1, 2, q.publish_at.strftime("%Y-%m-%d %H:%M:%S"))
+            sheet.write(i+1, 3, q.keys.name)
+        if not os.path.exists(MEDIA_ROOT):
+            os.mkdir(MEDIA_ROOT)
+        workbook.save(MEDIA_ROOT+'/news.xls')
+        return HttpResponseRedirect('/media/news.xls')
+    export_excel.short_description = u'导出Excel'
+
 xadmin.site.register(News, NewsAdmin)
